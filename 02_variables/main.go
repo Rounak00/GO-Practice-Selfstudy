b54f5f73de44fa1d42d44fe1a60e96c59ddc584a
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2025-05-22 12:08:46 +0530

    Create main.go

---
+++ 02_variables/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+function main(){
+   fmt.Println(1)
+   fmt.Println(true)
+   fmt.Println(1.364)
+   fmt.Println("Hello")
+
+
+//initialize of varible
+var a int = 10
+var b=10 //auto set the type not work if only declare but not initialize
+c:=true // short hand of declaration but out side of function not possible
+_____________________________________________________________________________
+//constant
+const a int = 10;
+const (
+       a=10
+       b=20
+     )
+fmt.Println(a)//10
+}
