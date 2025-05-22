40f35faf42d8d1d4ab78451c4f30b968abc7b28c
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2025-05-22 12:01:22 +0530

    Create main.go

---
+++ 01_HelloWorld/main.go
@@ -0,0 +1,24 @@
+/*
+why we need go ? 
+ - Fast Build time 
+ - Fast start up
+ - Perfomance and efficiency
+ - Concurrency module -> Go routines
+ - Static typing and compilation
+*/
+
+// Instal Go from do.dev. also install go extention from goggle in vscode
+
+
+package main //-> comes under the package group
+
+import "fmt"
+
+function main(){
+  fmt.Println("Hellowold")
+}
+
+
+//For run -> go run file_path
+// for build -> go build file_path
+
