ab6a702bddfca5308f18fe99080a512a45cfb6aa
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2025-05-22 13:24:26 +0530

    Create main.go

---
+++ 05_Arrays/main.go
@@ -0,0 +1,13 @@
+package main
+import "ftm"
+
+
+//numbered sequesnce of specific length
+function main(){
+   var nums [4]int 
+
+   nums[0] =1
+   fmt.Println(nums[0])//1
+   fmt.Println(nums)//[1 0 0 0]
+   fmt.Println(len(nums)) //4
+} 
