ef419a6a9270cbc40b1cf408823c2ee4e45f15ae
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2025-05-24 13:01:35 +0530

    Update main.go

--- 10_goRoutines/main.go
+++ 10_goRoutines/main.go
@@ -1,1 +1,17 @@
-txt
+//go routines are light wait threads
+
+func task(id int){
+ fmt.Println("Doint task ->", id);
+}
+func main(){
+  for i := 0; i< 10; i++ {
+    go task(i);//run it in a thread, go to schedular then call
+  }
+  time.Sleep(time.Second*2)// so main dont end before this task calls end
+}
+
+
+// we can also do this way
+go func(i int){
+  fmt.PRintln(i)
+}(i)
